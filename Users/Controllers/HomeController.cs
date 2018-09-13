6badc55f43bf552c95274d784dfe7941b1f90cad
Andrii Tomashevskyi <tomashevskiy.a@gmail.com>
2018-09-13 07:59:49 +0300

    Using Roles for Authorization

--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -7,9 +7,28 @@ namespace Users.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public ViewResult Index()
+        [Authorize]
+        public IActionResult Index()
         {
-            return View(new Dictionary<string, object> { ["Placeholder"] = "Placeholder" });
+            return View(GetData(nameof(Index)));
+        }
+
+        [Authorize(Roles = "Users")]
+        public IActionResult OtherAction()
+        {
+            return View("Index",GetData(nameof(OtherAction)));
+        }
+
+        private Dictionary<string, object> GetData(string actionName)
+        {
+            return new Dictionary<string, object>
+            {
+                ["Action"] = actionName,
+                ["User"] = HttpContext.User.Identity.Name,
+                ["Authenticated"] = HttpContext.User.Identity.IsAuthenticated,
+                ["Auth Type"] = HttpContext.User.Identity.AuthenticationType,
+                ["In Users Role"] = HttpContext.User.IsInRole("Users")
+            };
         }
     }
 }
